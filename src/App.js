3a7ad1f213470b2e8286dec13d080e1b783e0dcc
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-09-15 22:50:43 +0530

    Refactor and reformat the React application

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="container">
-				App
+      <div className='container'>
+        App
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
