730ca1de05fdaa068688f4305476fd67a4450f03
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-09-17 02:25:50 +0530

    implementing routes and navigations of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Login from './pages/Login'
-// import TaskApp from './pages/TaskApp'
+import TaskApp from './pages/TaskApp'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Login/>
-        {/*<TaskApp/>*/}
+        <Router>
+          <Switch>
+            <Route exact
+                   path='/'
+                   component={Login}/>
+            <Route exact
+                   path='/home'
+                   component={TaskApp}/>
+          </Switch>
+        </Router>
       </div>
     )
   }
