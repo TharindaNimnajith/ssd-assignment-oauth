66b1a374483e18a0112f841eff6024b41200805d
JanithRushantha <janithrushantha@gmail.com>
2021-09-17 00:19:43 +0530

    Front-end - Login implementation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
-import App from './App'
+// import App from './App'
+import Login from './components/Login'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<Login/>, document.getElementById('root'))
