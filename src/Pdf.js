c0a62a95d42066c905510a5814ab26c9c90e7a1f
NehanIlangakoon <nehanilangakoon1@gmail.com>
2021-09-17 23:32:33 +0530

    Pdf report creation

---
+++ src/Pdf.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
+function MyApp() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document
+        file="somefile.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
+    </div>
+  );
+}
