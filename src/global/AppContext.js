198c76231be9828219f159b00b8fa6aaee4eef55
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-09-18 00:21:31 +0530

    Implement react context api for global state management

--- src/global/AppContext.js
+++ src/global/AppContext.js
@@ -2,6 +2,7 @@ import {createContext} from 'react'
 
 export const AppContext = createContext({
   loginData: null,
+  isAuthenticated: false,
   login: async () => {
   },
   logout: async () => {
--- src/global/global-state/GlobalState.jsx
+++ src/global/global-state/GlobalState.jsx
@@ -1,20 +1,24 @@
 import React, {useState} from 'react'
-import {AppContext} from './app-context'
+import {AppContext} from '../AppContext'
 
 export const GlobalState = props => {
   const [loginData, setLoginData] = useState(null)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   const login = async data => {
     setLoginData(data)
+    setIsAuthenticated(true)
   }
 
   const logout = async () => {
     setLoginData(null)
+    setIsAuthenticated(false)
   }
 
   return (
     <AppContext.Provider value={{
       loginData: loginData,
+      isAuthenticated: isAuthenticated,
       login: login,
       logout: logout
     }}>
