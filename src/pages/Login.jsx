fed6b5d66ba015449ee8812d210e36989cb7dbe7
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-09-17 19:30:17 +0530

    Google Sign in functionality with react-google-login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,12 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import GoogleLogin from 'react-google-login'
 import background from '../images/background.jpg'
 
 export default class Login extends Component {
+  responseGoogle = response => {
+    console.log(response)
+  }
+
   render() {
     return (
       <div className='text-center d-flex h-100'
@@ -37,19 +41,24 @@ export default class Login extends Component {
               style={{
                 letterSpacing: '2px'
               }}>
-            Sign In with Google
+            Sign in with Google
           </h3>
           <div className='d-flex justify-content-center py-2'>
-            <Link to='home'>
-              <button type='button'
-                      className='text-uppercase btn btn-primary btn-block shadow-none px-4'>
-                <label className='text-decoration-none pt-2'>
-                  <i className='fab fa-google'/>
-                  &nbsp;
-                  Sign In With Google
-                </label>
-              </button>
-            </Link>
+            {/*<Link to='home'>*/}
+            {/*  <button type='button'*/}
+            {/*          className='text-uppercase btn btn-primary btn-block shadow-none px-4'>*/}
+            {/*    <label className='text-decoration-none pt-2'>*/}
+            {/*      <i className='fab fa-google'/>*/}
+            {/*      &nbsp;*/}
+            {/*      Sign in With Google*/}
+            {/*    </label>*/}
+            {/*  </button>*/}
+            {/*</Link>*/}
+            <GoogleLogin clientId='658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
+                         buttonText='Sign in with Google'
+                         onSuccess={this.responseGoogle}
+                         onFailure={this.responseGoogle}
+                         cookiePolicy={'single_host_origin'}/>
           </div>
         </div>
       </div>
