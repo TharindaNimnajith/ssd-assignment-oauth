a2e21f67736fac3b96fe2b9e9a98151e676b3cbf
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-09-18 05:31:28 +0530

    Retrieving google account info (name, email, profile pic)

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import {AppContext} from '../../global/AppContext'
 import {authStoreKey} from '../../config/config'
 import {removeFromLocalStorage} from '../../helpers/helpers'
+import icon from '../../images/todo_icon.png'
 
 export default class Profile extends Component {
   static contextType = AppContext
@@ -18,20 +19,36 @@ export default class Profile extends Component {
       <div className='card card-body my-3'>
         <div className='row'>
           <div className='col-1 align-self-center'>
-            <img src={this.context.loginData.profileObj.imageUrl}
-                 className='rounded-circle'
-                 alt='Google Account Profile'
-                 width='50px'
-                 height='50px'/>
+            {
+              this.context.loginData ? (
+                <img src={this.context.loginData.profileObj.imageUrl}
+                     className='rounded-circle'
+                     alt='Google Account Profile'
+                     width='50px'
+                     height='50px'/>
+              ) : (
+                <img src={icon}
+                     className='rounded-circle'
+                     alt='Google Account Profile'
+                     width='50px'
+                     height='50px'/>
+              )
+            }
           </div>
-          <div className='col-9 align-self-center'>
-            <p className='mb-0 font-weight-bold'>
-              Hello, {this.context.loginData.profileObj.name}!
-            </p>
-            <small>
-              {this.context.loginData.profileObj.email}
-            </small>
-          </div>
+          {
+            this.context.loginData ? (
+              <div className='col-9 align-self-center'>
+                <p className='mb-0 font-weight-bold'>
+                  Hello, {this.context.loginData.profileObj.name}!
+                </p>
+                <small>
+                  {this.context.loginData.profileObj.email}
+                </small>
+              </div>
+            ) : (
+              <div className='col-9 align-self-center'/>
+            )
+          }
           <div className='col-2 align-self-center'>
             <Link to='/'>
               <button type='button'
