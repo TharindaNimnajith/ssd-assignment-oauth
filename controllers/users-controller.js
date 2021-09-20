e765ae4043164c9fbb6fd73c61a36b0b39612c25
JanithRushantha <janithrushantha@gmail.com>
2021-09-20 16:58:06 +0530

    Backend routes and controller files

---
+++ controllers/users-controller.js
@@ -0,0 +1,17 @@
+const UserModel = require('../models/users.model')
+
+const addUser = async (req, res) => {
+
+}
+
+const updateUser = async (req, res) => {
+
+}
+
+const getUser = async (req, res) => {
+
+}
+
+exports.addUser = addUser
+exports.updateUser = updateUser
+exports.getUser = getUser
---
+++ routes/users.routes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const UsersController = require('../controllers/users-controller')
+
+const router = express.Router()
+
+module.exports = router
